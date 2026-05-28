6ce778ee002d8651bec341e578887f1fadfaff76
Nemenwq <chloemikka@gmail.com>
2026-05-28 12:06:33 +0100

    Implement set_admin Function

--- contracts/collateral-vault/src/events.rs
+++ contracts/collateral-vault/src/events.rs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 use soroban_sdk::{contractevent, Address};
 
 #[contractevent]
@@ -22,11 +23,18 @@ pub struct AssetRemoved {
 
 #[contractevent]
 #[derive(Clone, Debug, PartialEq)]
+=======
+use soroban_sdk::{contracttype, Address};
+
+#[contracttype]
+#[derive(Clone, Debug, PartialEq, Eq)]
+>>>>>>> ad858a3 (Implement set_admin Function)
 pub struct AdminChanged {
     pub old_admin: Address,
     pub new_admin: Address,
 }
 
+<<<<<<< HEAD
 #[contractevent]
 #[derive(Clone, Debug, PartialEq)]
 pub struct Paused {
@@ -37,4 +45,11 @@ pub struct Paused {
 #[derive(Clone, Debug, PartialEq)]
 pub struct Unpaused {
     pub paused: bool,
+=======
+#[contracttype]
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct AddCollateralEvent {
+    pub from: Address,
+    pub amount: i128,
+>>>>>>> ad858a3 (Implement set_admin Function)
 }
