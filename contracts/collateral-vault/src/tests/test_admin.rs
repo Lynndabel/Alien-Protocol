673c58e0e015291a7964a7dcbf87c8f198e67ee7
Nemenwq <chloemikka@gmail.com>
2026-05-30 11:17:45 +0100

    test: add snapshot for set_admin_same_address

--- contracts/collateral-vault/src/tests/test_admin.rs
+++ contracts/collateral-vault/src/tests/test_admin.rs
@@ -246,6 +246,5 @@ fn test_set_admin_same_address() {
     let (_env, client, admin, _user, _token_id, _token_client, _token_admin) = setup_env();
 
     let result = client.try_set_admin(&admin);
-    assert_eq!(result, Err(Ok(VaultError::AlreadyAdmin.into())));
+    assert_eq!(result, Err(Ok(VaultError::AlreadyAdmin)));
 }
-
